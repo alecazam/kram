3bccb452e53c8cd2ba44fbdd0a741747ad2f862b
Alec Miller <alecmiller@yahoo.com>
2021-01-11 19:22:36 -0800

    Scripts - correct script to fix launch services.

--- scripts/fixfinder.sh
+++ scripts/fixfinder.sh
@@ -1,8 +1,12 @@
 #!/bin/zsh
 
-# when dropping a new build of the app into /Applications, finder still runs an old cached copy
+set -x
+
+# When dropping a new build of the app into /Applications, finder still runs an old cached copy
 # this resets launch servies to run the new code.  Why is this even necessary?
 
+# reset launch services that hold the cached app 
 /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
 
+# reset finder, so that double clicking on ktx/png files launches the new app
 killall Finder
