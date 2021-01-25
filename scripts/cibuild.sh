3917717be665f00a43b43c04c9d59fc690a2ea9c
Alec Miller <alecmiller@yahoo.com>
2021-01-25 00:58:47 -0800

    Travis - add linux and windows targets, all with clang

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+buildType=$1
+
 set -x
 
 # create directories to install results
@@ -7,7 +9,16 @@ mkdir -p bin
 mkdir -p build
 
 pushd build
-cmake .. -G Xcode
+
+# can't just use cmake .., gets confused about metal files since language not recognized, but Xcode knows how to build these
+if [[ $buildType == osx ]]; then
+	cmake .. -G Xcode
+elif [[ $buildType == windows ]]; then
+	cmake .. -G "Visual Studio 15 2017" -A x64
+elif [[ $buildType == linux ]]; then
+	cmake .. 
+fi
+
 cmake --build . --config Release
 cmake --install . --config Release
 popd
