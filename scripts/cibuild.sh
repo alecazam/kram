90e193a534876c9edd5cc3dcdc3c970845e07a50
Alec Miller <alecmiller@yahoo.com>
2021-01-25 09:57:47 -0800

    Travis - more fixes for Win cmake

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -13,6 +13,17 @@ mkdir -p build
 
 pushd build
 
+# need cmake 3.19 for universal apps, but vms have 3.18
+# and 3.18 for Win, but machines only have 3.18 and 3.17 installed
+if [[ $buildType == osx ]]; then
+	# this is done in .travis file
+	# brew upgrade cmake
+elif [[ $buildType == windows ]]; then
+	choco upgrade cmake
+elif [[ $buildType == linux ]]; then
+	# TODO: add curl to some server
+fi
+
 # can't just use cmake .. on osx, cmake gets confused about metal files since language not recognized
 # but Xcode knows how to build these.  I don't to setup special rule for metal files right now.
 if [[ $buildType == osx ]]; then
@@ -25,4 +36,5 @@ fi
 
 cmake --build . --config Release
 cmake --install . --config Release
+
 popd
