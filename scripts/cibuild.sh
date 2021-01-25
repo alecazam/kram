4c70653eb37c39ef9f46da95186dc04eeb1b4bd9
Alec Miller <alecmiller@yahoo.com>
2021-01-25 09:36:49 -0800

    Travis - more fixes for Win, start of unix build in CMake files

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -1,5 +1,8 @@
-#!/bin/zsh
+#!/bin/bash
 
+# note: zsh works on TrivisCI osx, but not on Win git bash, so using bassh
+
+# osx, windows, linux
 buildType=$1
 
 set -x
@@ -10,7 +13,8 @@ mkdir -p build
 
 pushd build
 
-# can't just use cmake .., gets confused about metal files since language not recognized, but Xcode knows how to build these
+# can't just use cmake .. on osx, cmake gets confused about metal files since language not recognized
+# but Xcode knows how to build these.  I don't to setup special rule for metal files right now.
 if [[ $buildType == osx ]]; then
 	cmake .. -G Xcode
 elif [[ $buildType == windows ]]; then
