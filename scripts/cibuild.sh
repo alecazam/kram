aea972de48a7c1ee4b855c71feb43d04b2aa59c2
Alec Miller <alecmiller@yahoo.com>
2021-01-30 12:50:58 -0800

    CI - switch from Travis to Github Actions
    Zip app build, and have pre-release and release actions.
    Remove travis.yml

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -5,7 +5,22 @@
 # osx, windows, linux
 buildType=$1
 
-set -x
+# translate Github Actions to os typpe
+if [[ $buildType =~ "ubuntu" ]]; then
+	buildType=linux
+fi
+if [[ $buildType =~ "windows" ]]; then
+	buildType=windows
+fi
+if [[ $buildType =~ "ubuntu" ]]; then
+	buildType=linux
+fi
+
+# exit on failure of any command
+set -e
+
+# don't use this or secrets may appear in build log
+#set -x
 
 # create directories to install results
 mkdir -p bin
@@ -13,30 +28,27 @@ mkdir -p build
 
 pushd build
 
-# need cmake 3.19 for universal apps, but vms have 3.18
-# and 3.18 for Win, but machines only have 3.18 and 3.17 installed
-
-# this is done in travis
-#if [[ $buildType == osx ]]; then brew upgrade cmake; fi
-
-if [[ $buildType == windows ]]; then choco upgrade cmake; fi
-
-# TODO: do a curl to update cmake
-#if [[ $buildType == linux ]]; then docurl...; fi
-
-
-
 # can't just use cmake .. on osx, cmake gets confused about metal files since language not recognized
 # but Xcode knows how to build these.  I don't to setup special rule for metal files right now.
 if [[ $buildType == osx ]]; then
 	cmake .. -G Xcode
 elif [[ $buildType == windows ]]; then
-	cmake .. -G "Visual Studio 15 2017" -A x64
+	cmake .. -G "Visual Studio 15 2017 Win64"
+	#cmake .. -G "Visual Studio 15 2019" -A x64
 elif [[ $buildType == linux ]]; then
 	cmake .. 
 fi
 
+# build the release build
 cmake --build . --config Release
+
+# copy it to bin directory
 cmake --install . --config Release
+
+# now that bin folder has kramv.app, zip move it so it can be copied to releases
+# this should really use cpack in the make file to build a dmg, but not there yet
+if [[ $buildType == osx ]]; then
+	zip -rm bin/kramv.app
+fi
 
 popd
