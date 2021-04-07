2cb3a9dd1a7a570cf65c6dd566e976c8013d7079
Alec Miller <alecmiller@yahoo.com>
2021-04-07 14:44:17 -0700

    kram - fix main to return 1 on failures, KPS CFBundlePackageType set to 8BIF
    KPS plugin shows up in PS but "fails to find file" on open/save.

--- kram/KramMain.cpp
+++ kram/KramMain.cpp
@@ -2,5 +2,12 @@
 
 int main(int argc, char* argv[])
 {
-    return kram::kramAppMain(argc, argv);
+    int errorCode = kram::kramAppMain(argc, argv);
+    
+    // returning -1 from main results in exit code of 255, so fix this to return 1 on failure.
+    if (errorCode != 0) {
+        exit(1);
+    }
+    
+    return 0;
 }
