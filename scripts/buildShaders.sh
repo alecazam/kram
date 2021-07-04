ce91383a6d391383ba7e2b140dba76c38e90cb73
Alec Miller <alecmiller@yahoo.com>
2021-07-03 23:18:24 -0700

    kramv - remove the bin/.air file once the .metallib is built.

--- scripts/buildShaders.sh
+++ scripts/buildShaders.sh
@@ -2,3 +2,6 @@
 
 xcrun -sdk macosx metal -c ../kramv/KramShaders.metal -o ../bin/KramShaders.air
 xcrun -sdk macosx metallib ../bin/KramShaders.air -o ../bin/KramShaders.metallib
+
+# don't need this after metallib built
+rm ../bin/KramShaders.air
