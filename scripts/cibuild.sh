5967eca5a792e4757f118133a9919b9d2d46e721
Alec Miller <alecmiller@yahoo.com>
2021-08-22 12:24:08 -0700

    kram - move cibuild over to xcodebuild for macOS/iOS
    Trying to reduce dependence on CMake.

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -35,26 +35,56 @@ set -e
 
 # create directories to install results
 mkdir -p bin
+
+# need absolute path
+binPath=$(realpath bin)
+
 mkdir -p build
 
 pushd build
 
 # can't just use cmake .. on osx, cmake gets confused about metal files since language not recognized
-# but Xcode knows how to build these.  I don't to setup special rule for metal files right now.
+# but Xcode knows how to build these.  I don't want to setup special rule for metal files right now.
 if [[ $buildType == macos ]]; then
-	cmake .. -G Xcode
+
+	# not using CMake anymore on mac/iOS.  Using custom workspace and projects.
+	#cmake .. -G Xcode
+
+	# build the release build
+	#cmake --build . --config Release
+
+	# copy it to bin directory
+	#cmake --install . --config Release
+
+	pushd ../build2
+
+	xcodebuild build -sdk iphoneos -workspace kram.xcworkspace -scheme kram-ios CONFIGURATION_BUILD_DIR=${binPath} BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+	xcodebuild build -sdk macosx -workspace kram.xcworkspace -scheme kram CONFIGURATION_BUILD_DIR=${binPath} BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+	xcodebuild install -sdk macosx -workspace kram.xcworkspace -scheme kramc DSTROOT=${binPath} INSTALL_PATH=.
+	xcodebuild install -sdk macosx -workspace kram.xcworkspace -scheme kramv DSTROOT=${binPath} INSTALL_PATH=.
+
+	popd
+
 elif [[ $buildType == windows ]]; then
 	#cmake .. -G "Visual Studio 15 2017 Win64"
 	cmake .. -G "Visual Studio 16 2019" -A x64
+
+	# build the release build
+	cmake --build . --config Release
+
+	# copy it to bin directory
+	cmake --install . --config Release
+
 elif [[ $buildType == linux ]]; then
 	cmake .. 
+
+	# build the release build
+	cmake --build . --config Release
+
+	# copy it to bin directory
+	cmake --install . --config Release
 fi
-
-# build the release build
-cmake --build . --config Release
-
-# copy it to bin directory
-cmake --install . --config Release
 
 popd
 
