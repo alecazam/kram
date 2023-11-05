cf071f73e491fb4f7abe38b58fffc3f9d46ca126
Alec Miller <alecmiller@yahoo.com>
2023-11-05 11:17:44 -0800

    kram - add in vma visualizer dump, and added Perfetto python script
    This is untested.  But include sample from AMD.

---
+++ scripts/GpuMemDumpPerfetto.py
@@ -0,0 +1,322 @@
+# kram - Copyright 2020-2023 by Alec Miller. - MIT License
+# The license and copyright notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# This is derived from GpuMemDumpVis.py, and like it, doesn't handle aliasing.
+# Only reads size and not offset of allocations.  But Perfetto can't handle overlapping rects.
+# Unlike the png, Pefetto can zoom in and display the allocation names and sizes.
+
+#
+# Copyright (c) 2018-2023 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import argparse
+import json
+
+PROGRAM_VERSION = 'Vulkan/D3D12 Memory Allocator Dump Perfetto 3.0.3'
+
+# dx12 or vulkan
+currentApi = ""
+
+# input data dictionary
+data = {}
+
+ # now convert the dictonaries to new dictionaries, and then out to json 
+perfettoDict = {
+    'displayTimeUnit': 'ms', # TODO: /1000, not quite kb
+    'systemTraceEvents': 'systemTraceEvents',
+    'traceEvents': [],
+}
+
+def ParseArgs():
+    argParser = argparse.ArgumentParser(description='Visualization of Vulkan/D3D12 Memory Allocator JSON dump in Perfetto.')
+    argParser.add_argument('DumpFile', help='Path to source JSON file with memory dump created by Vulkan/D3D12 Memory Allocator library')
+    argParser.add_argument('-v', '--version', action='version', version=PROGRAM_VERSION)
+    # TODO: derive output from input name if not present
+    argParser.add_argument('-o', '--output', required=True, help='Path to destination trace file')
+    return argParser.parse_args()
+
+def GetDataForMemoryPool(poolTypeName):
+    global data
+    if poolTypeName in data:
+        return data[poolTypeName]
+    else:
+        newPoolData = {'DedicatedAllocations':[], 
+                       'Blocks':[], 
+                       'CustomPools':{}}
+        data[poolTypeName] = newPoolData
+        return newPoolData
+
+def ProcessBlock(poolData, block):
+    blockInfo = {'ID': block[0], 
+                 'Size': int(block[1]['TotalBytes']), 
+                 'Suballocations':[]}
+    for alloc in block[1]['Suballocations']:
+        allocData = {'Type': alloc['Type'], 
+                     'Size': int(alloc['Size']), 
+                     'Usage': int(alloc['Usage']) if 'Usage' in alloc else 0 }
+        blockInfo['Suballocations'].append(allocData)
+    poolData['Blocks'].append(blockInfo)
+    
+def IsDataEmpty():
+    global data
+    for poolData in data.values():
+        if len(poolData['DedicatedAllocations']) > 0:
+            return False
+        if len(poolData['Blocks']) > 0:
+            return False
+        for customPool in poolData['CustomPools'].values():
+            if len(customPool['Blocks']) > 0:
+                return False
+            if len(customPool['DedicatedAllocations']) > 0:
+                return False
+    return True
+
+def RemoveEmptyType():
+    global data
+    for poolType in list(data.keys()):
+        pool = data[poolType]
+        if len(pool['DedicatedAllocations']) > 0:
+           continue
+        if len(pool['Blocks']) > 0:
+            continue
+        empty = True
+        for customPool in pool['CustomPools'].values():
+            if len(customPool['Blocks']) > 0:
+                empty = False
+                break
+            if len(customPool['DedicatedAllocations']) > 0:
+                empty = False
+                break
+        if empty:
+            del data[poolType]
+
+def AllocTypeToCategory(type, usage):
+    global currentApi
+    if type == 'FREE':
+        return ""
+    elif type == 'UNKNOWN':
+        return "?"
+
+    if currentApi == 'Vulkan':
+        if type == 'BUFFER':
+            if (usage & 0x1C0) != 0: # INDIRECT_BUFFER | VERTEX_BUFFER | INDEX_BUFFER
+                isVB = True # TODO: split up
+                return ifelse(isVB, "VB", "IB") 
+            elif (usage & 0x28) != 0: # STORAGE_BUFFER | STORAGE_TEXEL_BUFFER
+                return "SB"
+            elif (usage & 0x14) != 0: # UNIFORM_BUFFER | UNIFORM_TEXEL_BUFFER
+                return "UB"
+            else:
+                return "?B"
+        elif type == 'IMAGE_OPTIMAL':
+            # TODO: need tex type (2d, 3d, ...)
+            if (usage & 0x20) != 0: # DEPTH_STENCIL_ATTACHMENT
+                return "DT"
+            elif (usage & 0xD0) != 0: # INPUT_ATTACHMENT | TRANSIENT_ATTACHMENT | COLOR_ATTACHMENT
+                return "RT"
+            elif (usage & 0x4) != 0: # SAMPLED
+                return "TT"
+            else:
+                return "?T"
+        elif type == 'IMAGE_LINEAR' :
+            return "LT"
+        elif type == 'IMAGE_UNKNOWN':
+            return "?T"
+    elif currentApi == 'Direct3D 12':
+        if type == 'BUFFER':
+            return "?B"
+        elif type == 'TEXTURE1D' or type == 'TEXTURE2D' or type == 'TEXTURE3D':
+            if (usage & 0x2) != 0: # D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL
+                return "DT"
+            elif (usage & 0x5) != 0: # D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET | D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS
+                return "RT"
+            elif (usage & 0x8) == 0: # Not having D3D12_RESOURCE_FLAG_DENY_SHARED_RESOURCE
+                return "TT"
+            else:
+                return "?T"
+    else:
+        print("Unknown graphics API!")
+        exit(1)
+    assert False
+    return "??"
+
+def AddTraceEventsAlloc(alloc, address, blockCounter):
+    global perfettoDict
+
+    size = alloc['Size']
+    type = AllocTypeToCategory(alloc['Type'], alloc['Usage'])
+
+    # begin/end events for Perfetto, address is treated as a time value for plotting rects
+    perfettoDict['traceEvents'].add({
+        'name': alloc['Name'], # TODO: conditionally add only if name present
+        'ph': 'B',
+        'ts': int(address),
+        'tid': int(blockCounter),
+        'cat': type
+    })
+    perfettoDict['traceEvents'].add(
+    {
+        'ph': 'E',
+        'ts': int(address+size),
+        'tid': int(blockCounter),
+        'cat': type # TODO: needed if begin already has it spec'd?
+    })
+
+def AddTraceEventsBlock(block, blockCounter):
+    global perfettoDict
+    address = 0
+    for alloc in block['Suballocations']:
+        AddTraceEventsAlloc(alloc, address, blockCounter)
+        address += alloc['Size']
+
+def AddBlockName(blockCounter, blockName):
+    global perfettoDict
+
+    # TODO: are these just loose added into the object ?
+    perfettoDict.add({
+        'name': 'thread_name',
+        'ph': 'M',
+        'tid': int(blockCounter),
+        'args': {
+            'name': blockName
+        }
+    })
+
+def AddTraceEvents():
+    global perfettoDict
+    blockCounter = 0
+    poolIndex = 0
+    for poolData in data.values():
+
+        # report for default pool
+
+        # block allocs
+        blockIndex = 0
+        for block in poolData['Blocks']:
+            blockName = "p{} block{} {}".format(poolIndex, blockIndex, block['ID'])
+            AddTraceEventsBlock(block, blockCounter)
+            AddBlockName(blockName, blockCounter)
+            blockCounter += 1
+            blockIndex += 1
+
+        # dedicated allocs
+        allocationIndex = 0
+        for dedicatedAlloc in poolData['DedicatedAllocations']:
+            blockName = 'p{} alloc{} {}'.format(poolIndex, allocationIndex, dedicatedAlloc['Name'])
+            AddTraceEventsAlloc(dedicatedAlloc, 0, blockCounter)
+            AddBlockName(blockName, blockCounter)
+            blockCounter += 1
+            allocationIndex += 1
+            
+        # repeat for custom pools
+        for customPoolData in poolData['CustomPools'].values():
+            customPoolName = customPoolData['Name']
+
+            # pool block allocs
+            blockIndex = 0
+            for block in customPoolData['Blocks']:
+                blockName = 'p{} {} block{} {}'.format(poolIndex, customPoolName, blockIndex, block['ID'])
+                AddTraceEventsBlock(block, blockCounter)
+                AddBlockName(blockName, blockCounter)
+                blockCounter += 1
+                blockIndex += 1
+
+            # pool dedicated allocs
+            allocationIndex = 0
+            for dedicatedAlloc in customPoolData['DedicatedAllocations']:
+                blockName = 'p{} {} alloc{} {}'.format(poolIndex, customPoolName, dedicatedAlloc['Name'])
+                AddTraceEventsAlloc(dedicatedAlloc, 0, blockCounter)
+                AddBlockName(blockName, blockCounter)
+                blockCounter += 1
+                allocationIndex += 1
+                
+        poolIndex += 1
+
+if __name__ == '__main__':
+    args = ParseArgs()
+    jsonSrc = json.load(open(args.DumpFile, 'rb'))
+ 
+    if 'General' in jsonSrc:
+        currentApi = jsonSrc['General']['API']
+    else:
+        print("Wrong JSON format, cannot determine graphics API!")
+        exit(1)
+            
+    # Process default pools
+    if 'DefaultPools' in jsonSrc:
+        for memoryPool in jsonSrc['DefaultPools'].items():
+            poolData = GetDataForMemoryPool(memoryPool[0])
+            # Get dedicated allocations
+            for dedicatedAlloc in memoryPool[1]['DedicatedAllocations']:
+                allocData = {'Type': dedicatedAlloc['Type'], 
+                             'Size': int(dedicatedAlloc['Size']), 
+                             'Usage': int(dedicatedAlloc['Usage'])}
+                poolData['DedicatedAllocations'].append(allocData)
+            # Get allocations in block vectors
+            for block in memoryPool[1]['Blocks'].items():
+                ProcessBlock(poolData, block)
+             
+    # Process custom pools
+    if 'CustomPools' in jsonSrc:
+        for memoryPool in jsonSrc['CustomPools'].items():
+            poolData = GetDataForMemoryPool(memoryPool[0])
+            for pool in memoryPool[1]:
+                poolName = pool['Name']
+                poolData['CustomPools'][poolName] = {'DedicatedAllocations':[], 'Blocks':[]}
+                # Get dedicated allocations
+                for dedicatedAlloc in pool['DedicatedAllocations']:
+                    allocData = {'Type': dedicatedAlloc['Type'], 
+                                 'Size': int(dedicatedAlloc['Size']), 
+                                 'Usage': int(dedicatedAlloc['Usage'])}
+                    poolData['CustomPools'][poolName]['DedicatedAllocations'].append(allocData)
+                # Get allocations in block vectors
+                for block in pool['Blocks'].items():
+                    ProcessBlock(poolData['CustomPools'][poolName], block)
+    
+    
+    if IsDataEmpty():
+        print("There is nothing to write. Please make sure you generated the stats string with detailed map enabled.")
+        exit(1)
+    RemoveEmptyType()
+    
+    AddTraceEvents()
+
+    perfettoJson = json.dumps(perfettoDict, indent=4)
+    
+    with open(args.output, "w") as outfile:
+        outfile.write(perfettoJson)
+    
+"""
+Main data structure - variable `data` - is a dictionary. Key is string - memory type name. Value is dictionary of:
+- Fixed key 'DedicatedAllocations'. Value is list of objects, each containing dictionary with:
+    - Fixed key 'Type'. Value is string.
+    - Fixed key 'Size'. Value is int.
+    - Fixed key 'Usage'. Value is int.
+- Fixed key 'Blocks'. Value is list of objects, each containing dictionary with:
+    - Fixed key 'ID'. Value is int.
+    - Fixed key 'Size'. Value is int.
+    - Fixed key 'Suballocations'. Value is list of objects as above.
+- Fixed key 'CustomPools'. Value is dictionary.
+  - Key is string with pool ID/name. Value is a dictionary with:
+    - Fixed key 'DedicatedAllocations'. Value is list of objects as above.
+    - Fixed key 'Blocks'. Value is a list of objects representing memory blocks as above.
+"""
