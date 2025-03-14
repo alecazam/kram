26aa3e6030fefe6ff9b70bce0f7f13ef5bddbe41
Alec Miller <alecmiller@yahoo.com>
2025-03-14 16:36:37 -0700

    kram - corrected testing of cpuInfo for AVX2
    The AI sample code was testing f16c, and assuming that was AVX2, but the 0 function returns the vendor ID.  Don't trust AI code.

--- kramc/KramMain.cpp
+++ kramc/KramMain.cpp
@@ -8,6 +8,8 @@
 #include <intrin.h>
 #endif
 
+using namespace STL_NAMESPACE;
+
 int main(int argc, char* argv[])
 {
     bool hasSimdSupport = true;
@@ -35,8 +37,6 @@ int main(int argc, char* argv[])
         hasSimdSupport = false;
     }
     else {
-        using namespace STL_NAMESPACE;
-        
         vector<char> buffer;
         buffer.resize(leaf7FeatureSize);
         sysctlbyname(avx2Features, buffer.data(), &leaf7FeatureSize, nullptr, 0);
@@ -53,17 +53,63 @@ int main(int argc, char* argv[])
     // Also handles Win for ARM (f.e. Prism is SSE4 -> AVX2 soon).
     // See here for more bits (f.e. AVX512)
     // https://learn.microsoft.com/en-us/cpp/intrinsics/cpuid-cpuidex?view=msvc-170
-    //
-    // ecx bit 28 is avx.
-    // ecx bit 29 is avx2
     
-    int cpuInfo[4] = {};
+    // f1.ecx bit  0 is sse3
+    // f1.ecx bit 19 is sse4.1
+    // f1.ecx bit 20 is sse4.2
+    // f1.ecx bit 28 is avx.
+    // f1.ecx bit 29 is f16c (docs are wrong about this being avx2)
+    
+    // f7.ebx bit 5 is avx2
+    // f7.ebx bit 16 is avx-512f
+    // f7.ebx bit 26 is avx-512pf
+    // f7.ebx bit 27 is avx-512er
+    // f7.ebx bit 28 is avx-512cd
+   
+   
+    // This returns a count of the ids from mthe docs.
+    struct CpuInfo {
+        int eax, ebx, ecx, edx;
+    };'
+    
+    // numIds in 0
+    // vendorId (12 char string) returned in 1,3,2
+    // can tell intel from amd off vendorId
+    CpuInfo cpuInfo = {};
     __cpuid(cpuInfo, 0);
     
-    #if SIMD_AVX2
-    bool hasAVX2 = cpuInfo[2] & (1 << 29);
-    hasSimdSupport = hasAVX2;
-    #endif
+    int numIds = cpuInfo[0];
+    if (numIds < 7) {
+        hasSimdSupport = false;
+    }
+    else {
+        // +1 since 0 is the count and vendorId
+        vector<CpuInfo> cpuInfoByIndex;
+        cpuInfoByIndex.resize(numIds+1);
+        
+        // This has sse4, avx, f16c
+        __cpuidex((int*)&cpuInfo, 1, 0);
+        cpuInfoByIndex[1] = cpuInfo;
+
+        // This has AVX2, avx512
+        __cpuidex((int*)&cpuInfo, 7, 0);
+        cpuInfoByIndex[7] = cpuInfo;
+        
+        // TODO: probably another for AVX10
+        
+// This is to obtain all the bits, but only care about AVX2
+//        for (int i = 0; i <= numIds; ++i) {
+//            __cpuidex((int*)&cpuInfo, i, 0); // get the values
+//            cpuInfoByIndex[i] = cpuInfo;
+//        }
+        
+        // there are extended bits above 0x8000000
+        
+        #if SIMD_AVX2
+        bool hasAVX2 = cpuInfoByIndex[7].ebx & (1 << 5);
+        hasSimdSupport = hasAVX2;
+        #endif
+    }
     
 #endif
     
