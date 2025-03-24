6c7a0f7ad12d7be61beedfa68142a7f00ba0f256
Alec Miller <alecmiller@yahoo.com>
2025-03-23 17:45:43 -0700

    kram - fix kramTests.sh to passthrough all args

--- scripts/kramTests.sh
+++ scripts/kramTests.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
-args=$1
+# return all args from $1 onward
+# can pass --force, -c ktx, -c dds, -c ktx2
+args=$@
 
 ../scripts/kramTextures.py -p mac --bundle ${args}
 #../scripts/kramTextures.py -p mac -c ktx --bundle ${args}
@@ -10,7 +12,7 @@ args=$1
 
 # this takes 15s+ with ETC2comp
 ../scripts/kramTextures.py -p android --bundle ${args}
-#../scripts/kramTextures.py -p -c ktx android --bundle ${args}
+#../scripts/kramTextures.py -p android -c ktx android --bundle ${args}
 
 # this only has ktx2 form, tests uastc which kram doesn't open/save yet
 #../scripts/kramTextures.py -p any --bundle ${args}
